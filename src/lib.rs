8fd7af046894e40e35a26bf48ac14a1d9af092d7
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-02-19 15:17:24 +0900

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-extern crate ic_agent;
 extern crate aws_sdk_kms;
+extern crate ic_agent;
 
-use k256::{
-    PublicKey,
-};
 use aws_sdk_kms::{primitives::Blob, types::SigningAlgorithmSpec, Client};
 use ic_agent::{export::Principal, Identity, Signature};
-
+use k256::{pkcs8::EncodePublicKey, PublicKey};
 
 pub struct KmsIdentity {
     client: Client,
@@ -21,19 +18,36 @@ impl KmsIdentity {
 
 impl Identity for KmsIdentity {
     fn sender(&self) -> Result<Principal, String> {
-        let der = PublicKey::from_sec1_bytes(self.public_key().unwrap().as_slice()).map_err(|e| e.to_string())?.to_public_key_der();
-        let pub_key =der.as_ref().map_err(|e| e.to_string())?;
+        let der = PublicKey::from_sec1_bytes(self.public_key().unwrap().as_slice())
+            .map_err(|e| e.to_string())?
+            .to_public_key_der();
+        let pub_key = der.as_ref().map_err(|e| e.to_string())?;
         Ok(Principal::self_authenticating(pub_key))
     }
 
     fn public_key(&self) -> Option<Vec<u8>> {
-        let request = self.client.get_public_key().key_id(self.key_id.clone()).send();
-        let out = futures::executor::block_on(request).map_err(|e| e.to_string()).unwrap();
+        let request = self
+            .client
+            .get_public_key()
+            .key_id(self.key_id.clone())
+            .send();
+        let out = futures::executor::block_on(request)
+            .map_err(|e| e.to_string())
+            .unwrap();
         out.public_key().map(|k| k.as_ref().to_vec())
     }
 
-    fn sign(&self, content: &ic_agent::agent::EnvelopeContent) -> Result<ic_agent::Signature, String> {
-        let request = self.client.sign().key_id(self.key_id.clone()).signing_algorithm(SigningAlgorithmSpec::EcdsaSha256).message(Blob::new( content.to_request_id().signable())).send();
+    fn sign(
+        &self,
+        content: &ic_agent::agent::EnvelopeContent,
+    ) -> Result<ic_agent::Signature, String> {
+        let request = self
+            .client
+            .sign()
+            .key_id(self.key_id.clone())
+            .signing_algorithm(SigningAlgorithmSpec::EcdsaSha256)
+            .message(Blob::new(content.to_request_id().signable()))
+            .send();
         let out = futures::executor::block_on(request).map_err(|e| e.to_string())?;
         let public_key = self.public_key().unwrap();
         let sig = Signature {
@@ -44,4 +58,3 @@ impl Identity for KmsIdentity {
         Ok(sig)
     }
 }
-
